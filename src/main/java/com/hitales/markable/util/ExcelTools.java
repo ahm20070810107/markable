fa6e9cfedb1ba4e53c40a9b5e94c5ed283e6efad
Hubot <ahm20070810107@163.com>
2018-05-02 15:31:05 +0800

    add package

--- src/main/java/com/hitales/markable/util/ExcelTools.java
+++ src/main/java/com/hitales/markable/util/ExcelTools.java
@@ -1,21 +1,17 @@
 package com.hitales.markable.util;
 
 import com.monitorjbl.xlsx.StreamingReader;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.*;
-import org.springframework.stereotype.Service;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
 
 
-@Service
-@Slf4j
 public class ExcelTools {
 
-    public  Workbook loadExcelFile(String excelPath){
+    public static  Workbook loadExcelFile(String excelPath){
 
         File excelFile = new File(excelPath);
         if(!excelFile.exists() || excelFile.isDirectory()){
@@ -36,37 +32,7 @@ public class ExcelTools {
         return workbook;
     }
 
-
-    public void readExcel(String path) {
-        Workbook wb = loadExcelFile(path);
-        Sheet sheet = wb.getSheetAt(0); //获取第1个sheet
-        Iterator<Row> rows = sheet.rowIterator();
-        Row row = rows.next();
-        //	int startCloumn = row.getFirstCellNum() + 1;//第一列不使用
-        int endCloumn = row.getLastCellNum();
-        while (rows.hasNext()) {
-            row = rows.next();
-            try {
-                String strJYFlag = getCellValue(row.getCell(2)).toString();
-                String strBZAnchor =getCellValue(row.getCell(1)).toString();
-                if (strJYFlag.equals("不禁用"))
-                    continue;
-                if(strBZAnchor.equals(""))
-                {
-                   // array.add(strBZAnchor);
-                }
-            }catch (Exception e){continue;}
-
-            for (int j = 4; j <= endCloumn; j++) {
-                Cell cell = row.getCell(j);
-                if (cell == null || cell.getCellType() == Cell.CELL_TYPE_BLANK) continue;
-                String str = getCellValue(cell).toString();
-              //  array.add(str);
-            }
-        }
-    }
-
-    public  Object getCellValue(Cell cell){
+    public static Object getCellValue(Cell cell){
         if(cell == null)return "";
         switch (cell.getCellType()) {
             case Cell.CELL_TYPE_STRING:
@@ -85,7 +51,6 @@ public class ExcelTools {
                 }
                 break;
         }
-
         return "";
     }
 }
