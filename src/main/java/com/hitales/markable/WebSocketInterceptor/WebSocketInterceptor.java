5f8a87bf89ebaee853059d51cfc5bf53650bf21d
Hubot <ahm20070810107@163.com>
2018-05-02 14:20:03 +0800

    webSocket

---
+++ src/main/java/com/hitales/markable/WebSocketInterceptor/WebSocketInterceptor.java
@@ -0,0 +1,48 @@
+package com.hitales.markable.WebSocketInterceptor;
+
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+/**
+ * @desp websocket拦截器
+ * @author huangming
+ *
+ */
+public class WebSocketInterceptor implements HandshakeInterceptor{
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                               WebSocketHandler handler, Exception exception) {
+
+    }
+
+    /**
+     * @desp 将HttpSession中对象放入WebSocketSession中
+     */
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request,
+                                   ServerHttpResponse response, WebSocketHandler handler,
+                                   Map<String, Object> map){
+        if(request instanceof ServletServerHttpRequest){
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+            HttpSession session = servletRequest.getServletRequest().getSession();
+            String path= servletRequest.getURI().getQuery();
+
+            if(session!=null){
+                //区分socket连接以定向发送消息
+                //     session.setAttribute("user",path);
+                map.put("user", path);
+            }
+        }
+        return true;
+    }
+
+}
