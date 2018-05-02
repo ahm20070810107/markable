a4a0c58cdb99b1104c12408a64e9eac5f85d5ed8
Hubot <ahm20070810107@163.com>
2018-05-02 14:06:03 +0800

    init

---
+++ src/main/java/com/hitales/markable/MarkableApplication.java
@@ -0,0 +1,12 @@
+package com.hitales.markable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkableApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hitales/markable/MarkableApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hitales.markable;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarkableApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
