305fcd7849589bba72d5a5a6319a44722e87b1d8
Hubot <ahm20070810107@163.com>
2018-05-10 13:41:10 +0800

    configer

--- src/main/java/com/hitales/markable/controller/DeleteController.java
+++ src/main/java/com/hitales/markable/controller/DeleteController.java
@@ -3,10 +3,7 @@ package com.hitales.markable.controller;
 import com.hitales.markable.Exception.BadRequestException;
 import com.hitales.markable.service.DeleteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sun.awt.SunHints;
 
 /**
@@ -22,8 +19,8 @@ public class DeleteController {
     @Autowired
     private DeleteService deleteService;
 
-    @DeleteMapping(value = "deleteFile")
-    public String deleteFile(@RequestParam(value = "fileName") String fileName){
+    @DeleteMapping(value = "/{fileName}")
+    public String deleteFile(@PathVariable String fileName){
 
         deleteService.deleteFileData(fileName);
         return String.format("Delete file Data %s OK!",fileName);
